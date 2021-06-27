b83fa356025bcdc48b8525caecb07ce21557bd5d
Matthew Pawlak <pharaohnof@gmail.com>
2021-06-27 14:31:05 -0400

    redid file structure -- added nodemon for dev

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,4 +3,6 @@ const path = require("path")
 module.exports = (app) => {
 
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/views/index.html')));
+
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/views/notes.html')));
 }
